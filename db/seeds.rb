ea0bfb8381ffa0429e33852f4ba6296ef0276845
Florent Guilleux <florent2@gmail.com>
2012-07-27 14:42:43 -0500

    Add a boilerplate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# this seed file is made to be used on an empty db, usually it is called through `rake db:reset`
+
+if Rails.env.development?
+
+  #puts "Creating users..."
+  #User.create!(
+    #email:                  "joe@example.com",
+    #password:               "secret",
+    #password_confirmation:  "secret"
+  #)
+  #User.create!({
+    #email:                  "admin@example.com",
+    #password:               "secret",
+    #password_confirmation:  "secret",
+    #admin: true
+  #}, without_protection: true)
+
+end

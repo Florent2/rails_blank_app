c34fd697c572081d9c8dc265fee57dce095d3124
Florent Guilleux <florent2@gmail.com>
2012-08-07 20:38:15 -0500

    Start implementing an easier way to create the app

--- README.md
+++ README.md
@@ -2,15 +2,18 @@ Blank Rails app
 ===
 
 This app contains just the minimum configuration needed to start coding right
-away on the things that matter. And it's easier to maintain than a rails
+away on the things that matter. It's easier for me to maintain than a rails
 template.
-
-I use this app with [this script](https://gist.github.com/3166714) to create new
-Rails applications.
 
 This is an adapted version of the [Blank Slate
 app](https://github.com/foca/blank_slate_rails_app). Thanks to
 [foca](https://github.com/foca) for this great idea!
+
+How to use
+---
+
+    $ git clone https://github.com/Florent2/rails_blank_app YourAppName
+    $ YourAppName/init.sh
 
 Customizations
 ---
---
+++ init.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+# Convert ThisIsACamelizedWord to this_is_a_camelized_word
+#
+# Usage:
+#   underscore ThisIsACamelizedWord
+underscore() {
+  echo "$@" | sed -e 's/\([A-Z\d]\)\([A-Z][a-z]\)/\1_\2/g' -e 's/\([a-z\d]\)\([A-Z]\)/\1_\2/g' | tr "A-Z-" "a-z_";
+}
+
+# Rename the Rails app in the current directory. Both the new and old names must
+# be provided in CamelCase. The script will find CamelCase and unders_scored
+# words to replace.
+#
+# Usage:
+#   rename_rails_app "OldName" "NewName"
+rename_rails_app() {
+  local old_name="$1";
+  local new_name="$2";
+
+  local underscored_old_name=$(underscore $old_name);
+  local underscored_new_name=$(underscore $new_name);
+
+  find_and_replace_app_name() {
+    for file in $(find .); do
+      sed -i "" -e "s/$1/$2/g" $file 2>/dev/null;
+    done
+  }
+
+  find_and_replace_app_name $old_name $new_name;
+  find_and_replace_app_name $underscored_old_name $underscored_new_name;
+
+  return 0;
+}
+
+# Change the Rails app secret.
+#
+# Usage:
+#   change_rails_app_secret
+change_rails_app_secret() {
+  local new_secret=$(rake secret);
+  sed -i "" -e "s/'.*'/'$new_secret'/" "config/initializers/secret_token.rb"
+}
+
+case $1 in
+  -h | --help)
+    usage
+    ;;
+  *)
+    name="$1"
+    dir=$(underscore $name)
+    ;;
+esac
+
+app_name=`dirname $0`
+cd "$app_name" &>/dev/null
+
+rm -rf .git
+
+echo "- Renaming the app to \"${app_name}\""
+rename_rails_app "RailsBlankApp" "$app_name"
+
+echo "- Changing the app secret"
+change_rails_app_secret
+
+echo "- Setting the right README"
+mv README_APP.md README.md
+
+echo "- Initializing the git repository"
+(git init; git add .; git commit -m "Initial commit") &>/dev/null
+
+#if [-d "~/.config/consular"]; then
+  #echo "- Creating consular config file"
+  #wget
+#fi
+
+echo "==> Everything is ready, get to work!"
